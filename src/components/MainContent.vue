c85c9406bcbf69e5c39edcf1d4919cdeaff6bf9a
AleksStek <aleksstek@yandex.ru>
2023-10-16 21:32:29 +0700

    simple markup

---
+++ src/components/MainContent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="content">
+    <div class="cocktaile-info">
+      <h1>The cocktaile title</h1>
+      <p>Any description</p>
+    </div>
+    <img 
+      class="img"
+      src="https://cdn2.hubspot.net/hubfs/322787/Mychefcom/images/BLOG/Header-Blog/photo-culinaire-pexels.jpg" 
+      alt="cocktaile image" 
+    >
+  </section>
+</template>
+
+<style scoped lang="scss">
+.content {
+  display: flex;
+  text-align: left;
+}
+.img {
+  max-width: 400px;
+}
+.cocktaile-info {
+  padding: 10px 20px;
+}
+</style>
---
+++ src/components/NavList.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+const menu = [
+  {
+    id: 1, 
+    name: 'margarita'
+  },
+  {
+    id: 2,
+    name: 'mojito'
+  },
+  {
+    id: 3,
+    name: 'rum and coke'
+  },
+  {
+    id: 4,
+    name: 'gin and tonic'
+  }
+]
+
+const isActive = (id: number) => {
+  return id === 1
+}
+</script>
+
+<template>
+  <nav>
+    <ul class="list">
+      <li 
+        v-for="item in menu" 
+        :key="item.id"
+        class="list-item" 
+        :class="isActive(item.id) ? 'active' : ''"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+.list {
+  max-height: 390px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 5px 5px 0 5px;
+  border-left: none;
+  border-right: none;
+}
+
+.list-item {
+  list-style: none;
+  background-color: rgba(0, 0, 0, 0.05);
+  background-image: 
+    linear-gradient(
+      90deg,
+      #FFD32E 10px,
+      #EEE 10px,
+      #EEE 11px,
+      transparent 11px);
+  padding: 10px 15px 10px 25px;
+  border: 1px solid #CCC;
+  box-shadow: inset 1px 1px 0 rgba(255, 255, 255, 0.5);
+  margin-bottom: 5px;
+  width: 100%;
+  cursor: pointer;
+  border-radius: 3px;
+
+  &.active {
+    background-color: rgba(0, 0, 0, 0.25);
+  }
+}
+</style>
